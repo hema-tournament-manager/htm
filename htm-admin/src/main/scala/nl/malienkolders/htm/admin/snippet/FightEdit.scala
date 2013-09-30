e113ebdb6e63f226de4a110ed8ef4e8ac39393d9
Tim de Jager <tdejager89@gmail.com>
2013-09-30 23:09:46 +0200

    added submit button and time inputs

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/FightEdit.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/FightEdit.scala
@@ -6,6 +6,8 @@ import http._
 import sitemap._
 import util.Helpers._
 import nl.malienkolders.htm.lib.model._
+import java.text.SimpleDateFormat
+import net.liftweb.http.js.JsCmds.Run
 
 object FightEdit {
 
@@ -14,8 +16,23 @@ object FightEdit {
   lazy val loc = menu.toLoc
 
   def render = {
+    
     val f = Fight.findByKey(FightEdit.loc.currentValue.map(_.param).get.toLong).get
-    val s = f.currentScore
+    val totalScore = f.currentScore
+    
+    val score = Score.create
+    score.diffA(totalScore.a)
+    score.diffB(totalScore.b)
+    
+    val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    
+    def process() {
+      f.scores.clear()
+      f.scores += score
+      f.save()
+      S.redirectTo("/tournaments/view/" + f.pool.foreign.get.round.foreign.get.tournament.is)
+    }
+    S.appendJs(Run("$('#timeStop').datepicker({dateFormat: 'yy-mm-dd hh:nn:ss'});"))
     ".red" #> (
       ".name *" #> f.fighterA.obj.get.name.is &
       ".club [title]" #> f.fighterA.obj.get.club.is &
@@ -24,8 +41,11 @@ object FightEdit {
         ".name *" #> f.fighterB.obj.get.name.is &
         ".club [title]" #> f.fighterB.obj.get.club.is &
         ".club *" #> f.fighterB.obj.get.clubCode.is) &
-        "name=scoreRed" #> SHtml.text(s.a.toString, s => s) &
-        "name=scoreBlue" #> SHtml.text(s.b.toString, s => s)
+        "name=scoreRed" #> SHtml.text(totalScore.a.toString, s => score.diffA(s.toInt)) &
+        "name=scoreBlue" #> SHtml.text(totalScore.b.toString, s => score.diffB(s.toInt)) &
+        "name=timeStart" #> SHtml.text("", s => f.timeStart(df.parse(s).getTime()), "type" -> "datetime") &
+        "name=timeStop" #> SHtml.text("", s => f.timeStop(df.parse(s).getTime())) &
+        "#doEdit" #> SHtml.onSubmitUnit(process)
 
   }
 
