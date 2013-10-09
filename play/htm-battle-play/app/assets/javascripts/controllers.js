99612e4bcc3a9ab6cdae65b3e6db1229e524120f
Jogchem Dijkstra <jogchem@gmail.com>
2013-10-09 10:09:06 +0200

    correct listing of fights

--- play/htm-battle-play/app/assets/javascripts/controllers.js
+++ play/htm-battle-play/app/assets/javascripts/controllers.js
@@ -3,8 +3,13 @@
 var BattleCtrl = function($scope, $timeout, appService) {
 	var _ = window._;
 	
+	$scope.arena = {name: "Arena 1"};
+	$scope.tournament = {name: "Longsword Open"};
+	$scope.round = {name: "Round 1"};
+	$scope.pool = {order: 3};
     $scope.fights = appService.generateFights(20);
     $scope.currentFight = 1;
+    $scope.fightsShowing = [1, 5];
     $scope.fight = _.find($scope.fights, function(f) { return f.index == $scope.currentFight; });
     
     $scope.timer = {'running': false, 'lastStart': -1, 'currentTime': 0};
@@ -23,23 +28,30 @@ var BattleCtrl = function($scope, $timeout, appService) {
     };
     
     $scope.fightsBefore = function() {
-    	return currentFight > 3;
+    	return $scope.fightsShowing[0] > 1;
+    };
+    
+    $scope.fightsAfter = function() {
+    	return $scope.fightsShowing[1] < $scope.fights.length;
     };
     
     $scope.beforeRangeFunction = function(item) {
-    	return item.index < $scope.currentFight - 2;
+    	return item.index < $scope.fightsShowing[0];
     };
     
     $scope.inRangeFunction = function(item) {
-    	return Math.abs(item.index - $scope.currentFight) < 3;
+    	return item.index >= $scope.fightsShowing[0] && item.index <= $scope.fightsShowing[1];
     };
     
     $scope.afterRangeFunction = function(item) {
-    	return item.index > $scope.currentFight + 2;
+    	return item.index > $scope.fightsShowing[1];
     };
     
     $scope.incCurrentFight = function() {
     	$scope.currentFight = $scope.currentFight + 1;
+    	$scope.fightsShowing[0] = Math.max($scope.currentFight - 2, 1);
+    	$scope.fightsShowing[1] = Math.min($scope.fightsShowing[0] + 4, $scope.fights.length);
+    	$scope.fightsShowing[0] = Math.max($scope.fightsShowing[1] - 4, 1);
     };
     
   };
