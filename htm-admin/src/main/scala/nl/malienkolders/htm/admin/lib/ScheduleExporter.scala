fe97b51937c2cb5f42df0bf2e5f7df0dd3962acf
Pieter Peeters <pwampeeters@gmail.com>
2013-10-14 20:02:55 +0200

    Added schedule exporter class Added download schedule button

---
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/ScheduleExporter.scala
@@ -0,0 +1,15 @@
+package nl.malienkolders.htm.admin.lib
+
+import nl.malienkolders.htm.lib.model.Tournament
+
+object ScheduleExporter {
+  
+  def doExport(tournament: Tournament) {
+    
+    
+    
+    
+    
+  }
+
+}
