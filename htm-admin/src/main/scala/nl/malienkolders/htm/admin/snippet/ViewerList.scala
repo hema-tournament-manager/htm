da772b41f1c7334f6d1f11657f020d2b03178440
Jogchem Dijkstra <jogchem@gmail.com>
2013-10-16 17:27:10 +0200

    arena selection in viewer list

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/ViewerList.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/ViewerList.scala
@@ -10,10 +10,11 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 class ViewerList {
-
+  
   def render = {
     var name = ""
     var url = ""
+      var arena = false
 
     def process(): Unit = {
       Viewer.create.alias(name).url(url).save
@@ -24,6 +25,8 @@ class ViewerList {
       val status = v.rest.ping
       ".viewer [class+]" #> (if (status) "success" else "danger") &
         ".name *" #> v.alias.get &
+        ".arena" #> Arena.findAll.map(a => 
+          "*" #> arenaCheck(a, v)) & 
         ".url *" #> v.url.get &
         ".status *" #> (if (status) "Up" else "Down") &
         ".actions *" #> SHtml.submit("Delete", () => Viewer.findByKey(v.id.get).foreach(_.delete_!), "class" -> "btn btn-default btn-sm")
@@ -33,4 +36,11 @@ class ViewerList {
       "name=add" #> SHtml.onSubmitUnit(process)
   }
 
+  def arenaCheck(a: Arena, v: Viewer) =
+    SHtml.ajaxCheckbox(v.arenas.contains(a), {b: Boolean => 
+      if (b) v.arenas += a else v.arenas -= a
+      v.save
+      S.redirectTo("")
+    }, "title" -> a.name.get)
+  
 }
