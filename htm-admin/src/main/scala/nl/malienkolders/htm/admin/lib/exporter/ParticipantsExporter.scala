6bed74dfa0819bc9a5418b4ba59f01385d6c456a
Jogchem Dijkstra <jogchem@gmail.com>
2013-10-19 16:39:52 +0200

    order the participant excel list by externalId

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/exporter/ParticipantsExporter.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/exporter/ParticipantsExporter.scala
@@ -8,7 +8,7 @@ object ParticipantsExporter extends ExcelExporter {
   def doExport(out: OutputStream): Unit = {
     val workbook = loadWorkbook("participants")
     val sheet = workbook.getSheetAt(0)
-    for ((p, i) <- Participant.findAll().zipWithIndex) {
+    for ((p, i) <- Participant.findAll().sortBy(_.externalId.get.toInt).zipWithIndex) {
       val row = sheet.getOrCreateRow(i + 1)
       row.getOrCreateCell(0).setCellValue(p.externalId.get)
       row.getOrCreateCell(1).setCellValue(p.name.get)
