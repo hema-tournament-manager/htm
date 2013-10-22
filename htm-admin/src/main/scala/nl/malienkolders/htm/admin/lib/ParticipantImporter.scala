c7ab6699d91c81b4a6465fe6c0813654e9adabfc
Jogchem Dijkstra <jogchem@gmail.com>
2013-10-22 11:42:17 +0200

    removed unused code from ParticipantImporter

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/ParticipantImporter.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/ParticipantImporter.scala
@@ -19,62 +19,6 @@ import nl.htm.importer.EventData
 
 object ParticipantImporter {
 
-  val tournamentNames = List(
-    "longsword_open" -> "Longsword Open",
-    "longsword_ladies" -> "Longsword Ladies",
-    "wrestling" -> "Wrestling",
-    "sabre" -> "Sabre",
-    "rapier_dagger" -> "Rapier & Dagger",
-    "sword_buckler" -> "Sword & Buckler")
-
-  def readTuplesFromFile(filename: String) = Source.fromInputStream(ParticipantImporter.getClass().getResourceAsStream(filename), "UTF-8").getLines().toList.map(line => line.split(" -> ") match {
-    case Array(code, name) => code -> name
-    case _ => "" -> ""
-  })
-
-  lazy val clubCode2Name = Map(readTuplesFromFile("clubcodes"): _*)
-
-  lazy val clubName2Code = clubCode2Name.map { case (c, n) => (n, c) }
-
-  lazy val replacements = Map(readTuplesFromFile("clubreplacements").map { case (o, r) => (o.toLowerCase(), r) }: _*)
-
-  def nameReplacements = Map(("9", "F. v. d. Bussche-H.") :: ParticipantNameMapping.findAll.map(pnm => (pnm.externalId.is -> pnm.shortName.is)).toList: _*)
-
-  def normalizeName(nameRaw: String) = {
-    def normalizePart(part: String) = {
-      val subparts = part.split("-")
-      subparts.map(sb => if (sb.length() > 3) sb.take(1).toUpperCase() + sb.drop(1).toLowerCase() else sb).mkString("-")
-    }
-    val name = nameRaw.replaceAll("\\s+", " ").trim()
-    val parts = name.split(" ").toList
-    parts.map(normalizePart _).mkString(" ")
-  }
-
-  def shortenName(name: String) = {
-    val allParts = name.split(" ")
-    val uppercasedParts = allParts.takeWhile(_.charAt(0).isUpper)
-    val initials = uppercasedParts.take(allParts.length - 1).map(_.charAt(0) + ".")
-    (initials.toList.mkString + " " + allParts.drop(initials.length).mkString(" ")).replace(" van ", " v. ").replace(" von dem ", " v.d. ").replace(" von ", " v. ")
-  }
-
-  def normalizeClub(clubRaw: String) = {
-    def uppercaseWord(word: String) = if (word.length() > 3 && !word.contains(".")) word.take(1).toUpperCase() + word.drop(1) else word
-    val club = clubRaw.replaceAll("\\s+", " ").trim()
-    println("club: " + club)
-    val replaced: String = replacements.getOrElse(club.toLowerCase(), club)
-    println("replaced: " + replaced)
-    val uppercased = replaced.split(" ").map(uppercaseWord _).mkString(" ")
-    println("uppercased: " + uppercased)
-    if (uppercased == "" || uppercased == "-")
-      ("", "")
-    else if (clubCode2Name.contains(uppercased))
-      (uppercased, clubCode2Name(uppercased))
-    else if (clubName2Code.contains(uppercased))
-      (clubName2Code(uppercased), uppercased)
-    else
-      ("", uppercased)
-  }
-
   def doImport(data: EventData) = {
 
     val noCountry = Country.find(By(Country.code2, "")).get
