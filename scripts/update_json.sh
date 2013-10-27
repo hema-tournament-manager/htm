b397ab7e4448d7e313b46e6537d16f5f126980a1
Mattias Ryrlén <mattiasr@op5.com>
2013-10-27 15:58:40 +0100

    script/update_json: make it accept url from cli
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

--- scripts/update_json.sh
+++ scripts/update_json.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 admin_port="8079"
-url="http://localhost:$admin_port/api/v1/status/all"
 json_file="htm.json"
+if [ $1 ]; then
+	url="$1"
+else
+	url="http://localhost:$admin_port/api/v1/status/all"
+fi
+
 if [ -x $json_file ]; then
 	rm $json_file
 fi
