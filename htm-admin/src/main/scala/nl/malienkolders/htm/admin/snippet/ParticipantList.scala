b3c1500595ae7252864d00d79a73677b0dcb2815
Jogchem Dijkstra <jogchem@gmail.com>
2013-10-28 22:16:37 +0100

    grouped download buttons together in participant list

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/ParticipantList.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/ParticipantList.scala
@@ -52,8 +52,9 @@ object ParticipantList {
 
     def createParticipantSubmit = SHtml.submit("create participant", createParticipant, "class" -> "btn btn-default")
 
-    "#download" #> SHtml.link("/download/participants", () => throw new ResponseShortcutException(downloadParticipantList), <span><span class="glyphicon glyphicon-download"></span> Participants</span>, "class" -> "btn btn-default pull-right") &
-    "#downloadClubs" #> SHtml.link("/download/clubs", () => throw new ResponseShortcutException(downloadClubsList), <span><span class="glyphicon glyphicon-download"></span> Clubs</span>, "class" -> "btn btn-default pull-right") &
+    ".downloadButton *" #> Seq(
+        SHtml.link("/download/participants", () => throw new ResponseShortcutException(downloadParticipantList), Text("Participants")),
+        SHtml.link("/download/clubs", () => throw new ResponseShortcutException(downloadClubsList), Text("Clubs"))) &
       "#countrySelect *" #> SHtml.ajaxSelectObj(cs, Empty, { c: Country =>
         val cmd = selectedParticipant.map(p => changeCountry(p, c)) openOr (Noop)
         selectedParticipant = Empty
