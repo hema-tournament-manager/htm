091312c80f887db0162a8717802e9cd7798fe7fa
Mattias Ryrlén <mattiasr@op5.com>
2013-10-28 11:11:20 +0100

    scripts/update_json: keep history of exports
    it's always good to be able to rollback to a prev working copy.
    not it's possible.
    
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

--- scripts/update_json.sh
+++ scripts/update_json.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 admin_port="8079"
 json_file="htm.json"
+save_dir="/tmp/htm_json"
 if [ $1 ]; then
 	url="$1"
 else
 	url="http://localhost:$admin_port/api/v1/status/all"
 fi
 
-if [ -x $json_file ]; then
-	rm $json_file
+if [ ! -d "$save_dir" ]; then
+	mkdir $save_dir
 fi
 
-curl -o $json_file -O $url
+pushd $save_dir
+if [ ! -d ".git" ]; then
+	curl -o $json_file -O $url
+	git init
+	git add $json_file
+	git commit -asm "Initial Commit"
+else
+	curl -o $json_file -O $url
+	git commit -asm "Saved for export"
+fi
 
+
+pushd
