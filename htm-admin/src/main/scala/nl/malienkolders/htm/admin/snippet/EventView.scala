f3be548e3d01cd6835f3526a43f970b64c72ef61
Jogchem Dijkstra <jogchem@gmail.com>
2014-01-09 22:49:43 +0100

    Times in Event View are time zone independent

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/EventView.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/EventView.scala
@@ -7,14 +7,11 @@ import util.Helpers._
 import scala.xml.NodeSeq
 import nl.malienkolders.htm.lib.model._
 import java.text.SimpleDateFormat
-import org.joda.time.DateTime
-import org.joda.time.LocalTime
-import org.joda.time.format.DateTimeFormat
-import org.joda.time.LocalDate
+import com.github.nscala_time.time.Imports._
 
 class EventView {
-  val datef = DateTimeFormat.forPattern("yyyy-MM-dd")
-  val fmt = DateTimeFormat.forPattern("HH:mm");
+  val datef = DateTimeFormat.forPattern("yyyy-MM-dd").withZone(DateTimeZone.UTC)
+  val fmt = DateTimeFormat.forPattern("HH:mm").withZone(DateTimeZone.UTC)
 
   def timeslotMatrix = {
     val timeslotsPerArena = ArenaTimeSlot.findAll().groupBy(_.arena.is)
@@ -27,15 +24,15 @@ class EventView {
     loop(timeslotsPerArena)
   }
 
-  def timeInput(t: ArenaTimeSlot, f: MappedLong[_]) = SHtml.text(new LocalTime(f.get).toString(fmt), s => {
-    f(LocalTime.parse(s, fmt).millisOfDay().get)
+  def timeInput(t: ArenaTimeSlot, f: MappedLong[_]) = SHtml.text(LocalTime.fromMillisOfDay(f.get).toString(fmt), s => {
+    f(fmt.parseLocalTime(s).millisOfDay().get)
     t.save()
   }, "class" -> "form-control input-sm")
 
   def render = {
-    val e = Event.findAll().headOption.getOrElse(Event.create.name("My Event " + DateTime.now().year().get()).saveMe())
+    val e = Event.findAll().headOption.getOrElse(Event.create.name("My Event " + DateTime.now.year().get()).saveMe())
     val ds = Day.findAll() match {
-      case Nil => Day.create.date(LocalDate.now().toDate().getTime()).saveMe() :: Nil
+      case Nil => Day.create.date(LocalDate.now.toDate.getTime).saveMe() :: Nil
       case days => days
     }
     val arenas = Arena.findAll()
@@ -44,7 +41,7 @@ class EventView {
       ".day" #> ds.zipWithIndex.map {
         case (d, i) =>
           ".dayTitle *" #> ("Day " + (i + 1)) &
-            ".date" #> SHtml.text(new LocalDate(d.date.get).toString(datef), s => d.date(LocalDate.parse(s, datef).toDate().getTime()).save(), "class" -> "date pull-right form-control input-sm") &
+            ".date" #> SHtml.text(new LocalDate(d.date.get).toString(datef), s => d.date(datef.parseLocalDate(s).toDate().getTime()).save(), "class" -> "date pull-right form-control input-sm") &
             ".arenaHeader" #> arenas.map(a => "* *" #> a.name.get) &
             ".timeslot" #> timeslotMatrix.map(ts =>
               ".arena" #> ts.map {
