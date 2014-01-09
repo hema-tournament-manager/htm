579ee2f1b34eb6766f0f628e810214446533785b
Jogchem Dijkstra <jogchem@gmail.com>
2014-01-09 23:28:11 +0100

    Made the pre-commit hook a bit more useful by canceling the commit when sources have been formatted by sbt compile

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-exec sbt compile
+VAR=$(sbt compile | egrep "Reformatted [0-9]+ Scala source")
+if [ ! -z "$VAR" ]; then
+	echo "You are trying to commit unformatted Scala source, shame on you!"
+	exit 1
+fi
