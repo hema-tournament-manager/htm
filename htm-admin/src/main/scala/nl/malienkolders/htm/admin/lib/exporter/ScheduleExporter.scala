8326559e7b5f148d08bbd5423c70a454a46186fd
Jogchem Dijkstra <jogchem@gmail.com>
2014-01-20 22:16:41 +0100

    Export of arena schedule is working as well

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/exporter/ScheduleExporter.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/lib/exporter/ScheduleExporter.scala
@@ -9,66 +9,70 @@ import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.Sheet
 import java.util.Date
+import nl.malienkolders.htm.admin.lib.Utils.TimeRenderHelper
+import org.apache.poi.ss.util.WorkbookUtil
 
 object ScheduleExporter extends ExcelExporter {
 
-  abstract sealed class PoolHeaderStyle
-  case object ShortHeaders extends PoolHeaderStyle
-  case object LongHeaders extends PoolHeaderStyle
-
+  case class Group(name: String, blocks: Seq[Block])
   case class Block(name: String, fights: Seq[Fight[_, _]])
 
-  def doExport(header: String, blocks: Seq[Block], headerStyle: PoolHeaderStyle, outputStream: OutputStream): Unit = {
+  def doExport(groups: Seq[Group], outputStream: OutputStream): Unit = {
     val workbook = loadWorkbook("schedule")
 
-    val sheet = workbook.getSheetAt(0)
-
-    sheet.getOrCreateRow(0).getOrCreateCell(0).setCellValue(header)
-
-    var i = 2
-
-    for (block <- blocks) {
-
-      if (i == 2) {
-        i = i + 1
-      }
-      val headerRow = sheet.getOrCreateRow(i)
-
-      headerRow.getOrCreateCell(0).setCellValue(block.name)
-      i = i + 1;
-
-      val fights = block.fights.groupBy(_.scheduled.foreign.isDefined)
-
-      for {
-        scheduledFights <- fights.get(true)
-        fight <- scheduledFights
-      } {
-        val row = sheet.getOrCreateRow(i)
-
-        row.getOrCreateCell(1).setCellValue(new Date(fight.scheduled.foreign.get.time.get))
-        renderFight(row, fight)
-
-        i = i + 1
-      }
-
-      for {
-        unscheduledFights <- fights.get(false)
-        fight <- unscheduledFights
-      } {
-        val row = sheet.getOrCreateRow(i)
-
-        row.getOrCreateCell(1).setCellValue("??:??")
-        renderFight(row, fight)
-
-        i = i + 1
-      }
-
+    while (workbook.getNumberOfSheets() < groups.size) {
+      workbook.cloneSheet(0)
     }
 
+    for ((group, s) <- groups.zipWithIndex) {
+      val sheet = workbook.getSheetAt(s)
+      workbook.setSheetName(s, WorkbookUtil.createSafeSheetName(group.name))
+      sheet.getOrCreateRow(0).getOrCreateCell(0).setCellValue(group.name)
+
+      var i = 2
+
+      for (block <- group.blocks) {
+
+        if (i == 2) {
+          i = i + 1
+        }
+        val headerRow = sheet.getOrCreateRow(i)
+
+        headerRow.getOrCreateCell(0).setCellValue(block.name)
+        i = i + 1;
+
+        val fights = block.fights.groupBy(_.scheduled.foreign.isDefined)
+
+        for {
+          scheduledFights <- fights.get(true)
+          fight <- scheduledFights
+        } {
+          val row = sheet.getOrCreateRow(i)
+
+          row.getOrCreateCell(1).setCellValue(fight.scheduled.foreign.get.time.get.hhmm)
+          renderFight(row, fight)
+
+          i = i + 1
+        }
+
+        for {
+          unscheduledFights <- fights.get(false)
+          fight <- unscheduledFights
+        } {
+          val row = sheet.getOrCreateRow(i)
+
+          row.getOrCreateCell(1).setCellValue("??:??")
+          renderFight(row, fight)
+
+          i = i + 1
+        }
+
+      }
+    }
     workbook.write(outputStream)
   }
 
-  private def renderFight(row: Row, fight: Fight[_, _]) = {
+  private def renderFight[F <: Fight[_, _]](row: Row, fight: F) = {
     val t = fight.tournament
     row.getOrCreateCell(0).setCellValue(fight match {
       case pf: PoolFight =>
@@ -86,11 +90,15 @@ object ScheduleExporter extends ExcelExporter {
   }
 
   def doExport(tournament: Tournament)(outputStream: OutputStream): Unit = {
-    doExport(tournament.name.get, tournament.phases.map(p => Block(p.name.get, p.fights)), ShortHeaders, outputStream)
+    doExport(Seq(Group(tournament.name.get, tournament.phases.map(p => Block(p.name.get, p.fights)))), outputStream)
   }
 
   def doExport(arena: Arena, onlyUnfinishedPools: Boolean)(outputStream: OutputStream) {
-    //TODO: fix arena export
+    doExport((arena.timeslotByDay.zipWithIndex.map {
+      case ((day, timeslots), i) =>
+        Group(s"Day ${i + 1} - ${arena.name}", timeslots.map(ts =>
+          Block(s"${ts.from.get.hhmm}-${ts.to.get.hhmm} ${ts.name.get}", ts.fights.map(_.fight.foreign.get))))
+    }).toSeq, outputStream)
   }
 
 }
