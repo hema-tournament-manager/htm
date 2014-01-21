9dd36edc26911c7623d426640567b6686fb0d70f
Jogchem Dijkstra <jogchem@gmail.com>
2014-01-21 22:12:57 +0100

    Source format

--- htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/TournamentView.scala
+++ htm-admin/src/main/scala/nl/malienkolders/htm/admin/snippet/TournamentView.scala
@@ -204,7 +204,7 @@ object TournamentView {
     def renderFights(fights: Seq[Fight[_, _]]) = ".fight" #> fights.map(f =>
       ".fight-title *" #> f.name.get &
         ".scheduled [name]" #> s"fight${f.id.get}" &
-        ".edit [href]" #> ("/fights/edit/"+f.phaseType.code+"/"+f.id.get.toString) & 
+        ".edit [href]" #> ("/fights/edit/" + f.phaseType.code + "/" + f.id.get.toString) &
         (f.cancelled.get match {
           case true =>
             ".scheduled [class+]" #> "label-danger" &
