edf0e16887b080796e1ca8cc3cf2f055471c80f4
Jogchem Dijkstra <jogchem@gmail.com>
2014-01-30 20:03:50 +0100

    HTMAPP-257 Changed importer to handle new format

--- htm-importer/src/main/scala/nl/htm/importer/emag/Emag2014Importer.scala
+++ htm-importer/src/main/scala/nl/htm/importer/emag/Emag2014Importer.scala
@@ -20,16 +20,19 @@ object Emag2014Importer extends Importer[EmagExcelSettings] {
 
     val headerRow = total.getRow(1)
 
-    val header = Map((for (i <- 1 to headerRow.getLastCellNum() - 1) yield (headerRow.getCell(i).getStringCellValue(), i)): _*)
+    val header = Map((for (i <- 0 to headerRow.getLastCellNum() - 1) yield (headerRow.getCell(i).getStringCellValue(), i)): _*)
 
-    val tournaments = Map(
-      "Steel" -> "albion",
-      "Longsword" -> "longsword",
-      "Rapier" -> "rapier",
-      "Dussack" -> "dussack")
+    val tournaments = List(
+      "longsword",
+      "rapier",
+      "dussack",
+      "albion")
 
     val clubs = Map(
       "" -> "",
+      "AMEK" -> "AMEK",
+      "Blood and Iron" -> "B&I",
+      "GHFS" -> "GHFS",
       "Maza de Plata" -> "MdP",
       "Krigerskole" -> "KS",
       "Caballeros de Ebano" -> "CdE",
@@ -38,21 +41,21 @@ object Emag2014Importer extends Importer[EmagExcelSettings] {
       "Phoebus Ferratus" -> "PF",
       "Kanaan" -> "KNN")
 
-    val subscriptionsRaw = for (rowIndex <- 2 to total.getLastRowNum() if total.getRow(rowIndex) != null && total.getRow(rowIndex).getCell(1) != null) yield {
+    val subscriptionsRaw = for (rowIndex <- 2 to total.getLastRowNum() if total.getRow(rowIndex) != null && total.getRow(rowIndex).getCell(0) != null) yield {
       val row = total.getRow(rowIndex)
       (Participant(
-        List(SourceId("EMAG", rowIndex.toString)),
+        List(SourceId("EMAG", row.getCell(header("#")).getNumericCellValue().toInt.toString)),
         normalizeName(row.getCell(header("Name"))),
         shortenName(normalizeName(row.getCell(header("Name")))),
         row.getCell(header("School")),
         clubs(row.getCell(header("School"))),
-        row.getCell(header("Country")),
-        "") -> (for (colIndex <- header("Tournaments") to row.getLastCellNum() if row.getCell(colIndex) != null) yield { tournaments(row.getCell(colIndex)) }).toList)
+        row.getCell(header("Country")).replace("?", ""),
+        "") -> (for (colIndex <- header("Longsword") to header("Albion") if row.getCell(colIndex) != null) yield { tournaments(colIndex - header("Longsword")) }).toList)
     }
 
     val subscriptions = subscriptionsRaw.filter(_._1.name.length() > 0)
 
-    EventData(2, subscriptions.map(_._1).toList, Nil, tournaments.values.map(t =>
+    EventData(2, subscriptions.map(_._1).toList, Nil, tournaments.map(t =>
       Tournament(t, t, t, t) -> subscriptions.filter(_._2.contains(t)).zipWithIndex.map {
         case ((p, s), i) =>
           Subscription(false, i + 1, 0, None) -> p
