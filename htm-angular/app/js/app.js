65a522eb3912a5570e758eea808af837b179e3be
mpkorstanje <rien.korstanje@gmail.com>
2015-03-23 17:51:23 +0100

    Attached controllers to partials

--- htm-angular/app/js/app.js
+++ htm-angular/app/js/app.js
@@ -2,22 +2,19 @@
 
 /* App Module */
 (function(){
-	angular.module('htm.App', ['ngRoute','ui.bootstrap','htm.ui','htm.api','htm.tournament','htm.welcome','ngMockE2E'])
+	angular.module('htm.App', ['ngRoute','ui.bootstrap','htm.ui','htm.api','htm.tournament','ngMockE2E'])
 
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
 			when('/tournament', {
 				templateUrl: '/partials/tournament-list.html',
-				controller: 'TournamentListCtrl',
 			}).
 			when('/tournament/:tournamentId', {
 				templateUrl: '/partials/tournament.html',
-				controller: 'TournamentCtrl',
 			}).
 			otherwise({
 				redirectTo: '/',
 				templateUrl: '/partials/welcome.html',
-				controller: 'WelcomeCtrl',
 			});
 	}]);
 
