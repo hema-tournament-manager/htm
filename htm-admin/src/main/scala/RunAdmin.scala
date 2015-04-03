36425dee3331a2c1c619d6d785da7139cacd5819
mpkorstanje <rien.korstanje@gmail.com>
2015-04-04 01:31:50 +0200

    Minor bug fix clean up

--- htm-admin/src/main/scala/RunAdmin.scala
+++ htm-admin/src/main/scala/RunAdmin.scala
@@ -1,3 +1,5 @@
+
+
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
 
--- htm-admin/src/main/scala/bootstrap/liftweb/Boot.scala
+++ htm-admin/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,22 +1,3 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
 package bootstrap.liftweb
 
 import net.liftweb._
@@ -59,9 +40,6 @@ class Boot {
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
 
-    // Use Lift's Mapper ORM to populate the database
-    // you don't need to use Mapper to use Lift... use
-    // any ORM you want
     Schemifier.schemify(true, Schemifier.infoF _,
       User,
       Country,
@@ -106,7 +84,7 @@ class Boot {
 
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
+    
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
       new Html5Properties(r.userAgent))
