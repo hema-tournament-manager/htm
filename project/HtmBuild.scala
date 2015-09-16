e7e8b3fc2485a9148e7a5788d85f941703938c6b
Jogchem Dijkstra <jogchem@gmail.com>
2015-09-16 21:15:11 +0200

    Bump version to 1.4.4

--- project/HtmBuild.scala
+++ project/HtmBuild.scala
@@ -71,7 +71,7 @@ object BuildSettings {
   val buildOrganization = "nl.malienkolders"
 
   val buildName = "HTM"
-  val buildVersion = "1.4.3"
+  val buildVersion = "1.4.4"
   val buildScalaVersion = "2.10.4"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
